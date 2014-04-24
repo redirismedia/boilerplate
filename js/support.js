c0a2173867310e29c16ed65eab27f8cc944cc43e
Andrew Lin <sleepy@gmail.com>
2014-04-24 13:39:12 -0700

    copy base files

---
+++ js/support.js
@@ -0,0 +1,400 @@
+/*
+    Avoid `console` errors in browsers that lack a console.
+*/
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+
+/*
+    Add IE8 support for addEventListener()
+*/
+(function() {
+  if (!Event.prototype.preventDefault) {
+    Event.prototype.preventDefault=function() {
+      this.returnValue=false;
+    };
+  }
+  if (!Event.prototype.stopPropagation) {
+    Event.prototype.stopPropagation=function() {
+      this.cancelBubble=true;
+    };
+  }
+  if (!Element.prototype.addEventListener) {
+    var eventListeners=[];
+    
+    var addEventListener=function(type,listener /*, useCapture (will be ignored) */) {
+      var self=this;
+      var wrapper=function(e) {
+        e.target=e.srcElement;
+        e.currentTarget=self;
+        if (listener.handleEvent) {
+          listener.handleEvent(e);
+        } else {
+          listener.call(self,e);
+        }
+      };
+      if (type=="DOMContentLoaded") {
+        var wrapper2=function(e) {
+          if (document.readyState=="complete") {
+            wrapper(e);
+          }
+        };
+        document.attachEvent("onreadystatechange",wrapper2);
+        eventListeners.push({object:this,type:type,listener:listener,wrapper:wrapper2});
+        
+        if (document.readyState=="complete") {
+          var e=new Event();
+          e.srcElement=window;
+          wrapper2(e);
+        }
+      } else {
+        this.attachEvent("on"+type,wrapper);
+        eventListeners.push({object:this,type:type,listener:listener,wrapper:wrapper});
+      }
+    };
+    var removeEventListener=function(type,listener /*, useCapture (will be ignored) */) {
+      var counter=0;
+      while (counter<eventListeners.length) {
+        var eventListener=eventListeners[counter];
+        if (eventListener.object==this && eventListener.type==type && eventListener.listener==listener) {
+          if (type=="DOMContentLoaded") {
+            this.detachEvent("onreadystatechange",eventListener.wrapper);
+          } else {
+            this.detachEvent("on"+type,eventListener.wrapper);
+          }
+          break;
+        }
+        ++counter;
+      }
+    };
+    Element.prototype.addEventListener=addEventListener;
+    Element.prototype.removeEventListener=removeEventListener;
+    if (HTMLDocument) {
+      HTMLDocument.prototype.addEventListener=addEventListener;
+      HTMLDocument.prototype.removeEventListener=removeEventListener;
+    }
+    if (Window) {
+      Window.prototype.addEventListener=addEventListener;
+      Window.prototype.removeEventListener=removeEventListener;
+    }
+  }
+})();
+
+/*
+ * Add IE8 Support for getElementsByClassName
+ */
+(function() {
+  if (!document.getElementsByClassName) {
+      var indexOf = [].indexOf || function(prop) {
+          for (var i = 0; i < this.length; i++) {
+              if (this[i] === prop) return i;
+          }
+          return -1;
+      };
+      getElementsByClassName = function(className,context) {
+          var elems = document.querySelectorAll ? context.querySelectorAll("." + className) : (function() {
+              var all = context.getElementsByTagName("*"),
+                  elements = [],
+                  i = 0;
+              for (; i < all.length; i++) {
+                  if (all[i].className && (" " + all[i].className + " ").indexOf(" " + className + " ") > -1 && indexOf.call(elements,all[i]) === -1) elements.push(all[i]);
+              }
+              return elements;
+          })();
+          return elems;
+      };
+      document.getElementsByClassName = function(className) {
+          return getElementsByClassName(className,document);
+      };
+      Element.prototype.getElementsByClassName = function(className) {
+          return getElementsByClassName(className,this);
+      };
+  }
+})();
+
+/*
+  Convenient logging function
+*/
+function _log(s) {
+  console.log(s);
+}
+
+
+/*
+ * Add classList support for IE < 10
+ * classList.js: Cross-browser full element.classList implementation.
+ * 2012-11-15
+ *
+ * By Eli Grey, http://eligrey.com
+ * Public Domain.
+ * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+ */
+
+/*global self, document, DOMException */
+
+/*! @source http://purl.eligrey.com/github/classList.js/blob/master/classList.js*/
+
+if (typeof document !== "undefined" && !("classList" in document.documentElement)) {
+
+(function (view) {
+
+"use strict";
+
+if (!('HTMLElement' in view) && !('Element' in view)) return;
+
+var
+    classListProp = "classList"
+  , protoProp = "prototype"
+  , elemCtrProto = (view.HTMLElement || view.Element)[protoProp]
+  , objCtr = Object
+  , strTrim = String[protoProp].trim || function () {
+    return this.replace(/^\s+|\s+$/g, "");
+  }
+  , arrIndexOf = Array[protoProp].indexOf || function (item) {
+    var
+        i = 0
+      , len = this.length
+    ;
+    for (; i < len; i++) {
+      if (i in this && this[i] === item) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  // Vendors: please allow content code to instantiate DOMExceptions
+  , DOMEx = function (type, message) {
+    this.name = type;
+    this.code = DOMException[type];
+    this.message = message;
+  }
+  , checkTokenAndGetIndex = function (classList, token) {
+    if (token === "") {
+      throw new DOMEx(
+          "SYNTAX_ERR"
+        , "An invalid or illegal string was specified"
+      );
+    }
+    if (/\s/.test(token)) {
+      throw new DOMEx(
+          "INVALID_CHARACTER_ERR"
+        , "String contains an invalid character"
+      );
+    }
+    return arrIndexOf.call(classList, token);
+  }
+  , ClassList = function (elem) {
+    var
+        trimmedClasses = strTrim.call(elem.className)
+      , classes = trimmedClasses ? trimmedClasses.split(/\s+/) : []
+      , i = 0
+      , len = classes.length
+    ;
+    for (; i < len; i++) {
+      this.push(classes[i]);
+    }
+    this._updateClassName = function () {
+      elem.className = this.toString();
+    };
+  }
+  , classListProto = ClassList[protoProp] = []
+  , classListGetter = function () {
+    return new ClassList(this);
+  }
+;
+// Most DOMException implementations don't allow calling DOMException's toString()
+// on non-DOMExceptions. Error's toString() is sufficient here.
+DOMEx[protoProp] = Error[protoProp];
+classListProto.item = function (i) {
+  return this[i] || null;
+};
+classListProto.contains = function (token) {
+  token += "";
+  return checkTokenAndGetIndex(this, token) !== -1;
+};
+classListProto.add = function () {
+  var
+      tokens = arguments
+    , i = 0
+    , l = tokens.length
+    , token
+    , updated = false
+  ;
+  do {
+    token = tokens[i] + "";
+    if (checkTokenAndGetIndex(this, token) === -1) {
+      this.push(token);
+      updated = true;
+    }
+  }
+  while (++i < l);
+
+  if (updated) {
+    this._updateClassName();
+  }
+};
+classListProto.remove = function () {
+  var
+      tokens = arguments
+    , i = 0
+    , l = tokens.length
+    , token
+    , updated = false
+  ;
+  do {
+    token = tokens[i] + "";
+    var index = checkTokenAndGetIndex(this, token);
+    if (index !== -1) {
+      this.splice(index, 1);
+      updated = true;
+    }
+  }
+  while (++i < l);
+
+  if (updated) {
+    this._updateClassName();
+  }
+};
+classListProto.toggle = function (token, forse) {
+  token += "";
+
+  var
+      result = this.contains(token)
+    , method = result ?
+      forse !== true && "remove"
+    :
+      forse !== false && "add"
+  ;
+
+  if (method) {
+    this[method](token);
+  }
+
+  return !result;
+};
+classListProto.toString = function () {
+  return this.join(" ");
+};
+
+if (objCtr.defineProperty) {
+  var classListPropDesc = {
+      get: classListGetter
+    , enumerable: true
+    , configurable: true
+  };
+  try {
+    objCtr.defineProperty(elemCtrProto, classListProp, classListPropDesc);
+  } catch (ex) { // IE 8 doesn't support enumerable:true
+    if (ex.number === -0x7FF5EC54) {
+      classListPropDesc.enumerable = false;
+      objCtr.defineProperty(elemCtrProto, classListProp, classListPropDesc);
+    }
+  }
+} else if (objCtr[protoProp].__defineGetter__) {
+  elemCtrProto.__defineGetter__(classListProp, classListGetter);
+}
+
+}(self));
+
+}
+
+
+/* 
+ * Add IE8 Support for CustomEvent 
+ */
+ /*
+(function () {
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   };
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+})();
+*/
+
+try{new CustomEvent('?')}catch(o_O){
+  /*!(C) Andrea Giammarchi -- WTFPL License*/
+  this.CustomEvent = function(
+    eventName,
+    defaultInitDict
+  ){
+ 
+    // the infamous substitute
+    function CustomEvent(type, eventInitDict) {
+      var event = document.createEvent(eventName);
+      if (type != null) {
+        initCustomEvent.call(
+          event,
+          type,
+          (eventInitDict || (
+            // if falsy we can just use defaults
+            eventInitDict = defaultInitDict
+          )).bubbles,
+          eventInitDict.cancelable,
+          eventInitDict.detail
+        );
+      } else {
+        // no need to put the expando property otherwise
+        // since an event cannot be initialized twice
+        // previous case is the most common one anyway
+        // but if we end up here ... there it goes
+        event.initCustomEvent = initCustomEvent;
+      }
+      return event;
+    }
+ 
+    // borrowed or attached at runtime
+    function initCustomEvent(
+      type, bubbles, cancelable, detail
+    ) {
+      this['init' + eventName](type, bubbles, cancelable, detail);
+      'detail' in this || (this.detail = detail);
+    }
+ 
+    // that's it
+    return CustomEvent;
+  }(
+    // is this IE9 or IE10 ?
+    // where CustomEvent is there
+    // but not usable as construtor ?
+    this.CustomEvent ?
+      // use the CustomEvent interface in such case
+      'CustomEvent' : 'Event',
+      // otherwise the common compatible one
+    {
+      bubbles: false,
+      cancelable: false,
+      detail: null
+    }
+  );
+}
+
+
+
+/* 
+ * Add IE8 Support for createEvent
+ */
